b6ecda85824195d489d745bb786226af8525513d
Nikhila R <nikhila123@gmail.com>
2026-02-24 22:40:22 +0530

    modified index.js

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express=require("express");
 const mongoose=require("mongoose");
 const bodyParser=require("body-parser");
-const userRoutes=require("./routes/user1");
+const userRoutes=require("./routes/User1");
 const blogRoutes=require("./routes/blog2")
 const cors=require("cors")
 const app=express();
